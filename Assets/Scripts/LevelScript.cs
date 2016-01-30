9dd25c2e6d4bf3066ff65e4582fb3855fc8ef0db
Gleb Lebedev <gleb@gleblebedev.com>
2016-01-30 21:20:50 +0000

    script that generates boxes

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class LevelScript : MonoBehaviour {
 
+	public GameObject CubeA;
+
 	// Use this for initialization
 	void Start () {
-	
+		CreateBox(CubeA, new Vector3(-40,0,0));
+		CreateBox (CubeA, new Vector3(40,0,0));
+		CreateBox (CubeA, new Vector3(0,-40,0));
+		CreateBox (CubeA, new Vector3(0,40,0));
+	}
+
+	void CreateBox(GameObject prefab, Vector3 pos)
+	{
+		var c = (GameObject)Instantiate (prefab, pos, Quaternion.identity);
+		c.transform.SetParent (gameObject.transform);
+		c.transform.localPosition = pos;
+		c.transform.localRotation = Quaternion.identity;
 	}
 	
 	// Update is called once per frame
