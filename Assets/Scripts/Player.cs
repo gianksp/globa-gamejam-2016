2b1b57526007762d1a56658afd4c9be0b1081a2c
Giancarlo Sanchez <g.sanchez@glofox.com>
2016-01-30 23:14:26 +0000

    Latest color config for players

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,15 +7,35 @@ public class Player : NetworkItem {
 	private Vector3 _targetPosition;
 	private Quaternion _targetRotation;
 	public GameObject head;
+	public Color[] colors = new Color[10];
+	public Renderer renderer;
+
+	/// <summary>
+	/// Awake this instance.
+	/// </summary>
+	public void Awake() {
+		colors [0] = Color.black;
+		colors [1] = Color.white;
+		colors [2] = Color.red;
+		colors [3] = Color.green;
+		colors [4] = Color.blue;
+		colors [5] = Color.cyan;
+		colors [6] = Color.magenta;
+		colors [7] = Color.yellow;
+		colors [8] = Color.gray;
+		colors [9] = Color.grey;
+	}
 
 	/// <summary>
 	/// Init player / others
 	/// </summary>
 	public void Start() {
 		base.Start ();
+		renderer = gameObject.GetComponent<Renderer> ();
 		if (_isPlayer == true) {
 			Input.gyro.enabled = true;
 			head.SetActive (true);
+			renderer.material.color = colors[UnityEngine.Random.Range(0,colors.Length-1)];
 		} else {
 			gameObject.GetComponent<Rigidbody> ().isKinematic = true;
 		}
@@ -67,6 +87,11 @@ public class Player : NetworkItem {
 			_targetRotation    = JsonUtility.FromJson<Quaternion> (_properties["rotation"].ToString());
 			transform.rotation = Quaternion.Lerp(transform.rotation, _targetRotation, Time.deltaTime*5f);
 
+			object color;
+			if (_properties.TryGetValue("color",out color)) {
+				renderer.material.color = JsonUtility.FromJson<Color> (color.ToString());
+			} 
+
 		} catch (Exception ex) { }
 	}
 
@@ -76,6 +101,7 @@ public class Player : NetworkItem {
 	public void SetValues() {
 		_properties ["position"] = JsonUtility.ToJson (transform.position);
 		_properties ["rotation"] = JsonUtility.ToJson (transform.rotation);
+		_properties ["color"] = JsonUtility.ToJson (renderer.material.color);
 	}
 		
 }
