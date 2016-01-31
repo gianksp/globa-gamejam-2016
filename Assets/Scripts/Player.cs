098e6247ddc1e1ace7cd0ada8d3f4e1b4c2ba021
Giancarlo Sanchez <g.sanchez@glofox.com>
2016-01-31 05:15:35 +0000

    Attempt VR UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,9 @@ public class Player : NetworkItem {
 	public Color[] colors = new Color[10];
 	public Renderer renderer;
 	public Rigidbody rb;
+	public Light light;
+	public float timeRemaining = 90f;
+	public bool win = false;
 
 	/// <summary>
 	/// Awake this instance.
@@ -33,14 +36,21 @@ public class Player : NetworkItem {
 	public void Start() {
 		base.Start ();
 		rb = GetComponent<Rigidbody> ();
+		light = GetComponent<Light> ();
 		renderer = gameObject.GetComponent<Renderer> ();
 		if (_isPlayer == true) {
 			Input.gyro.enabled = true;
 			head.SetActive (true);
-			renderer.material.color = colors[UnityEngine.Random.Range(0,colors.Length-1)];
+//			renderer.material.color = colors[UnityEngine.Random.Range(0,colors.Length-1)];
+			InvokeRepeating("decreaseTimeRemaining", 1.0f, 1.0f);
 		} else {
 			gameObject.GetComponent<Rigidbody> ().isKinematic = true;
 		}
+	}
+
+	void decreaseTimeRemaining()
+	{
+		timeRemaining --;
 	}
 
 	/// <summary>
@@ -68,8 +78,18 @@ public class Player : NetworkItem {
 		if (_isPlayer) {
 			//Control motion
 			MoveByGamepad ();
+		
+			//Time is up
+			if (timeRemaining <= 0 && !win) {
+				//You lose
+				Application.LoadLevel("Over");
+			} else if (win) {
+				//You win
+				Application.LoadLevel("Win");
+			}
 		}
 
+		light.intensity = 0.2f+Mathf.PingPong (Time.time*0.6f, 0.8f);
 	}
 
 	public float speed = 2.0F;
@@ -78,7 +98,7 @@ public class Player : NetworkItem {
 		var v = Input.GetAxis ("Vertical")*speed;
 		var h = Input.GetAxis ("Horizontal")*speed;
 		transform.Translate (head.transform.forward * v * Time.deltaTime,Space.World);
-		transform.Translate (head.transform.right * h * Time.deltaTime,Space.Self);
+		transform.Translate (head.transform.right * h * Time.deltaTime,Space.World);
 
 		// Get our local Y+ vector (relative to world)
 		Vector3 localUp = transform.up ;
@@ -89,6 +109,7 @@ public class Player : NetworkItem {
 		GetComponent<Rigidbody>().MoveRotation (Quaternion.Slerp (rb.rotation, vertical, Time.deltaTime * 5f)) ;
 
 	}
+
 	/// <summary>
 	/// Based on the values stored in "SetValues" use them to update this gameobject instance
 	/// </summary>
@@ -104,10 +125,10 @@ public class Player : NetworkItem {
 			_targetRotation    = JsonUtility.FromJson<Quaternion> (_properties["rotation"].ToString());
 			transform.rotation = Quaternion.Lerp(transform.rotation, _targetRotation, Time.deltaTime*5f);
 
-			object color;
-			if (_properties.TryGetValue("color",out color)) {
-				renderer.material.color = JsonUtility.FromJson<Color> (color.ToString());
-			} 
+//			object color;
+//			if (_properties.TryGetValue("color",out color)) {
+//				renderer.material.color = JsonUtility.FromJson<Color> (color.ToString());
+//			} 
 
 		} catch (Exception ex) { }
 	}
@@ -118,7 +139,7 @@ public class Player : NetworkItem {
 	public void SetValues() {
 		_properties ["position"] = JsonUtility.ToJson (transform.position);
 		_properties ["rotation"] = JsonUtility.ToJson (transform.rotation);
-		_properties ["color"] = JsonUtility.ToJson (renderer.material.color);
+//		_properties ["color"] = JsonUtility.ToJson (renderer.material.color);
 	}
 		
 
